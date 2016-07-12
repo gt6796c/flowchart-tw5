1a5cec8fb3f0ab5b6b1b0ae71aece029fdd5f4e9
Nathan Jones <nbjones@gmail.com>
2016-07-12 01:20:32 -0400

    added js-seqence-diagram wrapper as well.

--- README.md
+++ README.md
@@ -8,6 +8,48 @@ place <$flowchart> widget and then flowchart language in between tags.
 #TODO
 1. ~~better error handling for preview~~
 2. ~~pass in flowstates to widget~~
-3. Can't handle hyperlinks in flowchart due to what looks like a parsing bug. Probably a conflict between TW5 syntax and flowchart's (escaping the forward slashes seems to make it possible to link to external sites)
-4. allow wiki links to be used in flowchart.
+3. ~~Can't handle hyperlinks in flowchart due to what looks like a parsing bug. Probably a conflict between TW5 syntax and flowchart's (escaping the forward slashes seems to make it possible to link to external sites)~~
+3.a. reversed the parsing for the couple of cases that this fails in. brittle.
+4. ~~allow wiki links to be used in flowchart.~~
+4.a used permalinks. kinda works ok
 5. make external links go create new tab and not use TW's.
+6. see if exports can be fixed. look at the highlight.js widget's tiddlywiki.files file for hint
+
+<pre>
+{
+      "x": 0,
+      "y": 0,
+      "line-width": 3,
+      "line-length": 50,
+      "text-margin": 10,
+      "font-size": 14,
+      "font-color": "black",
+      "line-color": "black",
+      "element-color": "black",
+      "fill": "white",
+      "yes-text": "yes",
+      "no-text": "no",
+      "arrow-end": "block",
+      "scale": 1,
+      // style symbol types
+      "symbols": {
+        "start": {
+          "font-color": "red",
+          "element-color": "green",
+          "fill": "yellow"
+        },
+        "end":{
+          "class": "end-element"
+        }
+      },
+      // even flowstate support ;-)
+      "flowstate" : {
+        "past" : { "fill" : "#CCCCCC", "font-size" : 12},
+        "current" : {"fill" : "yellow", "font-color" : "red", "font-weight" : "bold"},
+        "future" : { "fill" : "#FFFF99"},
+        "request" : { "fill" : "blue"},
+        "invalid": {"fill" : "#444444"},
+        "approved" : { "fill" : "#58C4A3", "font-size" : 12, "yes-text" : "APPROVED", "no-text" : "n/a" },
+        "rejected" : { "fill" : "#C45879", "font-size" : 12, "yes-text" : "n/a", "no-text" : "REJECTED" }
+      }
+</pre>
--- wrapper.js
+++ wrapper.js
@@ -16,6 +16,8 @@ module-type: widget
         window.eve = require("$:/plugins/gt6796c/flowchart-tw5/eve.js");
         window.Raphael = require("$:/plugins/gt6796c/flowchart-tw5/raphael.js");
         window.flowchart = require("$:/plugins/gt6796c/flowchart-tw5/flowchart.js");
+        window._ = require("$:/plugins/gt6796c/flowchart-tw5/underscore.js");
+        window.Diagram = require("$:/plugins/gt6796c/flowchart-tw5/sequence-diagram.js");
     }
 
     var FlowchartWidget = function(parseTreeNode, options) {
@@ -24,6 +26,122 @@ module-type: widget
 
     FlowchartWidget.prototype = new Widget();
 
+    function getScriptBody(src,tag)
+    {
+        var scriptBody="";
+        if (src.parseTreeNode.children)
+        {
+            var kids = src.parseTreeNode.children;
+            for (var k in kids)
+            {
+                var kk = kids[k];
+                switch(kk.type)
+                {
+                    // no wikiparsing found
+                    case "text":
+                        scriptBody += kk.text; break;
+                    // internal link, make it a permalink reference
+                    case "link":
+                        scriptBody += "#" + kk.children[0].text; break;
+                    // -- gets interpreted as an HTML element, we don't want that
+                    case "entity":
+                        switch(kk.entity)
+                        {
+                            case "&ndash;":
+                                scriptBody+="--"; break;
+                        };
+                        break;
+                    // just re-write it back out
+                    case "element":
+                        switch (kk.tag)
+                        {
+                            case "a":
+                                scriptBody+=kk.children[0].text;
+                        }
+                };
+            }
+        }
+        else if (src.parseTreeNode.text) {
+            scriptBody = src.parseTreeNode.text;
+        }
+        else {
+            scriptBody = "";
+        }
+        
+        return scriptBody;
+
+    };
+
+    function getCanvas(src,tag)
+    {
+        var height = src.getAttribute("height");
+        var width = src.getAttribute("width");
+
+        var divNode = src.document.createElement("div");
+        var style = "";
+        if (height) style +=" height:"+height;
+        if (width) style +=" width:"+width;
+        if (style) divNode.setAttribute("style", style);
+        divNode.setAttribute("id", tag+"_" + uniqueID);
+
+        uniqueID++;
+        return divNode;
+    };
+
+    function getOptions(src, tag, options)
+    {
+        try {
+            // try to set options from fields on tiddler first
+            // [tag-xxx]
+            var tt = src.getVariable('currentTiddler');
+            if (tt) {
+                var t = src.wiki.getTiddler(tt);
+                if (t)
+                {
+                    for (var f in t.fields) {
+                        var fi = f.indexOf(tag);
+                        if (fi != 0) continue;
+                        var k = f.substring(tag.length+1);
+                        var v = t.fields[f];
+                        console.error(k + ":" + v);
+                        // try as JSON
+                        try {
+                            options[k] = JSON.parse(v);
+                        }
+                            // last attempt is just a string
+                        catch (ex) {
+                            options[k] = v;
+                        }
+                    }
+                }
+            }
+            // treat any attributes as JSON representations of options
+            // for the object
+            for (var att in src.attributes) {
+                // we already used these in the div definition
+                if (att == "height" || att == "width") continue;
+
+                var attval = src.getAttribute(att);
+                // allow for data from named tiddlers
+                if ($tw.wiki.tiddlerExists(attval))
+                {
+                    var data = $tw.wiki.getTiddlerData(attval);
+                    options[att] = data;
+                }
+                else {
+                    // try as JSON
+                    try { options[att] = JSON.parse(attval); }
+                    // last attempt is just a string
+                    catch (ex) { options[att] = attval; }
+                }
+            }
+        }
+        catch (ex) { console.error(ex); }
+
+        return options;
+
+    };
+
     /*
      Render this widget into the DOM
      */
@@ -31,55 +149,29 @@ module-type: widget
         this.parentDomNode = parent;
         this.computeAttributes();
         this.execute();
-        
-        var height = this.getAttribute("height", "500px");
-        var width = this.getAttribute("width", "500px");
-        
-        var scriptBody = this.parseTreeNode.children[0].text;
-        var divNode = this.document.createElement("div");
-        var options = {};
-        divNode.setAttribute("style", "height:" + height + ";width:" + width);
-        divNode.setAttribute("id", "canvas_" + uniqueID);
+
+        var tag = 'flowchart';
+        var scriptBody = getScriptBody(this,tag);
+        var divNode = getCanvas(this,tag);
         parent.insertBefore(divNode, nextSibling);
         try {
-            // treat any attributes as JSON representations of options
-            // for the flow chart
-            try {
-                for (var att in this.attributes) {
-                    // we already used these in the div definition
-                    if (att == "height" || att == "width") continue;
-                    
-                    var attval = this.getAttribute(att);
-                    // allow for data from named tiddlers
-                    if ($tw.wiki.tiddlerExists(attval))
-                    {
-                        var data = $tw.wiki.getTiddlerData(attval);
-                        options[att] = data;
-                    }
-                    else {
-                        options[att] = JSON.parse(attval);
-                    }
-                }
-            }
-            catch (ex) { console.error(ex); }
+            var options = {};
+            getOptions(this, tag, options);
             var diagram = flowchart.parse(scriptBody);
-            diagram.drawSVG('canvas_' + uniqueID, options); 
+            diagram.drawSVG(divNode.id, options);
         }
         catch(ex)
         {
             divNode.innerText = ex;
         }
-        
 
-        uniqueID++;
-        
         this.domNodes.push(divNode);
     };
 
     FlowchartWidget.prototype.execute = function() {
         // Nothing to do
     };
-    
+
     /*
      Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
      */
@@ -88,5 +180,50 @@ module-type: widget
     };
 
     exports.flowchart = FlowchartWidget;
+
+    var SequenceDiagramWidget = function(parseTreeNode, options) {
+        this.initialise(parseTreeNode, options);
+    };
+
+    SequenceDiagramWidget.prototype = new Widget();
+    
+    /*
+     Render this widget into the DOM
+     */
+    SequenceDiagramWidget.prototype.render = function(parent,nextSibling) {
+        this.parentDomNode = parent;
+        this.computeAttributes();
+        this.execute();
+        
+        var tag = 'seqdiag';
+        var scriptBody = getScriptBody(this,tag);
+        var divNode = getCanvas(this,tag);
+        parent.insertBefore(divNode, nextSibling);
+        try {
+            var options = {'theme' : 'simple'};
+            getOptions(this, tag, options);
+            var diagram = Diagram.parse(scriptBody);
+            diagram.drawSVG(divNode.id, options); 
+        }
+        catch(ex)
+        {
+            divNode.innerText = ex;
+        }
+        
+        this.domNodes.push(divNode);
+    };
+
+    SequenceDiagramWidget.prototype.execute = function() {
+        // Nothing to do
+    };
+    
+    /*
+     Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+     */
+    SequenceDiagramWidget.prototype.refresh = function(changedTiddlers) {
+        return false;
+    };
+
+    exports.seqdiag = SequenceDiagramWidget;
     
 })();
